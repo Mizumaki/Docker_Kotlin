2d39a5515599a3824a0c1a196c6039000f675159
Mizumaki <statham.programming@gmail.com>
2019-06-16 21:15:33 +0900

    add ktor template

---
+++ ktor_api_sample/ktor-demo/src/Application.kt
@@ -0,0 +1,17 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module() {
+  routing {
+    get("/") {
+      call.respondText("Hello World!", ContentType.Text.Plain)
+    }
+  }
+}
